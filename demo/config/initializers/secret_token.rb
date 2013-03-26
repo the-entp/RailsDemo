e4ac884cf453dde7798c482554099104e4b568dc
Anna Shi <annashi.ys13@gmail.com>
2013-03-26 00:23:33 -0500

    Finished Hello World App

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'cbb41233e8f399a3cc8612d4ac1ff55c1fea965322887e01c9da0b04741adb481244732d4e2393ef6d2fd50d42bdd3daefa08621feeb7a2a8fc34701ff12e11a'
