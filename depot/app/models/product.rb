379d7ced29a32e1b5d2586c4c9742b7fb50bf30e
Anna Shi <annashi.ys13@gmail.com>
2013-03-27 23:17:59 -0500

    Added a store model

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  default_scope :order => "title"
   validates_presence_of :title, :description, :image_url
   validates_numericality_of :price
   validates_uniqueness_of :title
@@ -7,6 +8,7 @@ class Product < ActiveRecord::Base
   						:message => 'must be a URL for GIF, JPG, or PNG image.'
   validate :price_must_be_at_least_a_cent
   attr_accessible :title, :description, :image_url, :price
+
 
   protected
   	def price_must_be_at_least_a_cent
